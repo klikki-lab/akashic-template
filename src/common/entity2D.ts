6671434f95a0d7ccbbe01fb828027e3fed165f6d
klikki-lab <klikki.lab@gmail.com>
2025-04-16 14:09:35 +0900

    メソッドに変更

--- src/common/entity2D.ts
+++ src/common/entity2D.ts
@@ -3,11 +3,11 @@
  */
 export class Entity2D extends g.E {
 
-    top = (): number => this.y - this.height * this.anchorY;
+    top(): number { return this.y - this.height * this.anchorY; }
 
-    right = (): number => this.x + this.width * (1 - this.anchorX);
+    right(): number { return this.x + this.width * (1 - this.anchorX); }
 
-    bottom = (): number => this.y + this.height * (1 - this.anchorY);
+    bottom(): number { return this.y + this.height * (1 - this.anchorY); }
 
-    left = (): number => this.x - this.height * this.anchorX;
+    left(): number { return this.x - this.height * this.anchorX; }
 }
