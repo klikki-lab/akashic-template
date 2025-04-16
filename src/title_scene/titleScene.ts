2c1a3f473b2219a0f01f5d2c9dda48a834d40e4c
klikki-lab <klikki.lab@gmail.com>
2025-04-16 11:38:37 +0900

    各シーンにCountdownTimerを追加

--- src/title_scene/titleScene.ts
+++ src/title_scene/titleScene.ts
@@ -1,7 +1,12 @@
 import { BaseScene } from "../common/baseScene";
+import { CountdownTimer } from "../common/countdownTimer";
 import { GameMainParameterObject } from "./../parameterObject";
 
 export class TitleScene extends BaseScene<void> {
+
+    private countdownTimer: CountdownTimer;
+
+    private isFinish = true;
 
     constructor(param: GameMainParameterObject, timeLimit: number) {
         super({
@@ -11,10 +16,29 @@ export class TitleScene extends BaseScene<void> {
             ],
         });
 
-        this.onLoad.add(this.loadHandler);
+        this.onLoad.add(() => this.loadHandler(timeLimit));
     }
 
-    private loadHandler = (): void => {
+    private loadHandler = (timeLimit: number): void => {
+        this.countdownTimer = this.createCountdownTimer(timeLimit);
+    };
 
+    private updateHandler = (): void | boolean => {
+        if (this.isFinish) {
+            this.onFinish();
+            return true;
+        }
+    };
+
+    private createCountdownTimer = (timeLimit: number): CountdownTimer => {
+        const countdownTimer = new CountdownTimer(timeLimit);
+        countdownTimer.onTick = remainingSec => {
+
+        };
+
+        countdownTimer.onFinish = () => {
+            this.isFinish = true;
+        };
+        return countdownTimer;
     };
 }
