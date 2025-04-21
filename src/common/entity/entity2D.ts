6a0ad4d6b81d1b78e3d30b9ecad9158aa261f77d
klikki-lab <klikki.lab@gmail.com>
2025-04-21 23:42:23 +0900

    entity2D.tsを追加

---
+++ src/common/entity/entity2D.ts
@@ -0,0 +1,34 @@
+import * as GeometryUtil from "./geometryUtil";
+
+export class Entity2D extends g.E {
+
+    /**　
+        * @returns スケールを考慮した横幅。
+        */
+    getScaledWidth(): number { return GeometryUtil.getScaledWidth(this); }
+
+    /**
+     * @returns スケールを考慮した縦幅。
+     */
+    getScaledHeight(): number { return GeometryUtil.getScaledHeight(this); }
+
+    /** 
+     * @returns 上座標。
+     */
+    getTop(): number { return GeometryUtil.getTop(this); }
+
+    /**
+     * @returns 右座標。
+     */
+    getRight(): number { return GeometryUtil.getRight(this); }
+
+    /**
+     * @returns 下座標。
+     */
+    getBottom(): number { return GeometryUtil.getBottom(this); }
+
+    /**
+     * @returns 左座標。
+     */
+    getLeft(): number { return GeometryUtil.getLeft(this); }
+}
