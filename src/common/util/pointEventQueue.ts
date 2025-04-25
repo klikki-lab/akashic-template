2712d56f9b7b819d3f76d12d8ddbddc8d339d56f
klikki-lab <klikki.lab@gmail.com>
2025-04-26 00:01:19 +0900

    pointEventQueue.tsを追加

---
+++ src/common/util/pointEventQueue.ts
@@ -0,0 +1,28 @@
+export class PointEventQueue {
+
+    private queue: g.PointEvent[];
+
+    constructor() { 
+        this.queue = [];
+    }
+
+    push(event: g.PointEvent): void {
+        this.queue.push(event);
+    }
+
+    pop(): g.PointEvent | undefined {
+        return this.queue.shift();
+    }
+
+    clear(): void {
+        this.queue = [];
+    }
+
+    isEmpty(): boolean {
+        return this.queue.length === 0;
+    }
+
+    length(): number {
+        return this.queue.length;
+    }
+}
