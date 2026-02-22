35a78864ddcd22860992f73a44110a34032d71fd
klikki-lab <klikki.lab@gmail.com>
2026-02-22 09:26:15 +0900

    akashic-hover-plugin の実験

--- sandbox.config.js
+++ sandbox.config.js
@@ -1,6 +1,16 @@
 module.exports = {
-  "warn": {
-    "es6": false
+  displayOptions: {
+    fitsToScreen: false,
+    // backgroundImage: "./path/to/image.png",
+    // backgroundColor: "red",
+    showsGrid: false,
+    showsProfiler: true,
+    showsDesignGuideline: false,
   },
-  "showMenu": true,
+  warn: {
+    useDate: true,
+    useMathRandom: true,
+    drawOutOfCanvas: true
+  },
+  showMenu: true,
 };
--- src/game_scene/gameScene.ts
+++ src/game_scene/gameScene.ts
@@ -1,3 +1,5 @@
+import * as hover from "@akashic-extension/akashic-hover-plugin";
+import { TextButton } from "../common/gui/textButton";
 import { BaseScene } from "../common/scene/baseScene";
 import { AudioController } from "../common/util/audioController";
 import { CountdownTimer } from "../common/util/countdownTimer";
@@ -21,12 +23,55 @@ export class GameScene extends BaseScene<void> {
         });
 
         this.random = new Random(param.random || g.game.random);
+        // g.game.operationPluginManager.register(hover.HoverPlugin, 5);
+        g.game.operationPluginManager.register(hover.HoverPlugin, 25, { showTooltip: true });
+        // g.game.operationPluginManager.register(hover.HoverPlugin, 5, { cursor: "help", showTooltip: true }); // HoverPlugin を 識別コード 5 で 登録
+        g.game.operationPluginManager.start(25); // 識別コード 5 のプラグインを開始
+
+        // g.game.operationPluginManager.stop(5)
+
         this.onLoad.add(() => this.loadHandler(isTouched, timeLimit));
     }
 
     private loadHandler = (isTouched: boolean, timeLimit: number): void => {
         this.countdownTimer = this.createCountdownTimer(timeLimit);
         this.audioController = this.createAudioController(0.175, 0.2, !isTouched);
+
+
+        const font = new g.DynamicFont({
+            game: g.game,
+            fontFamily: "sans-serif",
+            size: 24,
+        });
+        const button = new TextButton(this, "START!", font);
+        button.x = (g.game.width - button.width) / 2;
+        button.y = (g.game.height - button.height) / 2;
+        // hover.Converter.asHoverable(button);
+        const hoveredRect = hover.Converter.asHoverable(button);
+        // hoveredRect.title= "Start Game";
+        // hoveredRect.hoverable = true;
+        hoveredRect.hovered.add(() => {
+            console.log("hovered");
+        });
+        hoveredRect.unhovered.add(() => {
+            console.log("unhovered");
+        });
+        this.append(button);
+
+        const button1 = new TextButton(this, "START!", font);
+        button1.x = (g.game.width - button.width) / 2;
+        button1.y = (g.game.height - button.height) / 2 + button.height * 2;
+        const hoveredRect1 = hover.Converter.asHoverable(button1);
+        hoveredRect1.title = "Test";
+        hoveredRect1.hoverable = true;
+        hoveredRect1.hovered.add(() => {
+            console.log("hovered");
+        });
+        hoveredRect1.unhovered.add(() => {
+            console.log("unhovered");
+        });
+
+        this.append(button1);
 
         this.onUpdate.add(this.updateHandler);
     };
@@ -40,7 +85,7 @@ export class GameScene extends BaseScene<void> {
     private processPointEvent(): void {
         if (!this.pointEventQueue.isEmpty()) {
             const ev = this.pointEventQueue.pop();
-            switch (ev.type) {
+            switch (ev?.type) {
                 case "point-down":
                     console.log("down");
                     break;
