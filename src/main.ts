b7590e73653184e0e40e1866e42b3a9bbef5038a
klikki-lab <klikki.lab@gmail.com>
2026-08-14 16:20:29 +0900

    main.tsを更新

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { CustomLoadingScene } from "./common/scene/customLoadingScene";
 import { GameScene } from "./game_scene/gameScene";
 import { GameMainParameterObject } from "./parameterObject";
+import { TitleScene } from "./title_scene/titleScene";
+
+const TimeLimit = { TITLE: 60, GAME: 10 };
 
 export function main(param: GameMainParameterObject): void {
     g.game.vars.gameState = {
@@ -10,11 +13,19 @@ export function main(param: GameMainParameterObject): void {
     };
     g.game.loadingScene = new CustomLoadingScene();
 
-    // const titleScene = new TitleScene(param, 10);
-    // titleScene.onFinish = isTouched => {
-    //     g.game.replaceScene(new GameScene(param, isTouched, 60));
-    // }
-    // g.game.pushScene(titleScene);
+    g.game.pushScene(createGameScene(param, true));
+}
 
-    g.game.pushScene(new GameScene(param, true, 60));
+function createTitleScene(param: GameMainParameterObject): TitleScene {
+    const titleScene = new TitleScene(param, TimeLimit.TITLE);
+    titleScene.onFinish = isTouched =>
+        g.game.replaceScene(createGameScene(param, isTouched));
+    return titleScene;
+}
+
+function createGameScene(param: GameMainParameterObject, isTouched: boolean): GameScene {
+    const gameScene = new GameScene(param, isTouched, TimeLimit.GAME);
+    gameScene.onFinish = () =>
+        g.game.replaceScene(createTitleScene(param));
+    return gameScene;
 }
