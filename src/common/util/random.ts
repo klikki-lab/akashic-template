e5696e5c5f5da6810782eb9308c6792f254bcfe6
klikki-lab <klikki.lab@gmail.com>
2026-08-14 16:31:18 +0900

    不正な引数が渡された場合、エラーを投げるように修正

--- src/common/util/random.ts
+++ src/common/util/random.ts
@@ -12,7 +12,7 @@ export class Random {
 
     /** 
      * @param max 最大値
-     * @returns `0` 以上 `max` 未満のランダム値を返す。
+     * @returns `0` 以上 `max` 未満のランダム値を返す。`max` が `0` 以下の場合は `0` 以下 `max` 未満のランダム値を返す。
      */
     generate(max: number): number;
 
@@ -20,6 +20,7 @@ export class Random {
      * @param min 最小値
      * @param max 最大値
      * @returns `min` 以上 `max` 未満のランダム値を返す。
+     * @throws `min` が `max` 以上の場合はエラーを投げる。
      */
     generate(min: number, max: number): number;
 
@@ -30,12 +31,15 @@ export class Random {
         if (arg2 === undefined) {
             return this.random.generate() * arg1;
         }
+        if (arg1 >= arg2) {
+            throw new Error("arg1 must be less than arg2.");
+        }
         return this.random.generate() * (arg2 - arg1) + arg1;
     }
 
     /** 
      * @param max 最大値
-     * @returns `0` 以上 `max` 未満のランダム整数を返す。
+     * @returns `0` 以上 `max` 未満のランダム整数を返す。`max` が `0` 以下の場合は `0` 以下 `max` 未満のランダム整数を返す。
      */
     generateInt(max: number): number;
 
@@ -43,12 +47,16 @@ export class Random {
      * @param min 最小値
      * @param max 最大値
      * @returns `min` 以上 `max` 未満のランダム整数を返す。
+     * @throws `min` が `max` 以上の場合はエラーを投げる。
      */
     generateInt(min: number, max: number): number;
 
-    generateInt(arg1?: number, arg2?: number): number {
+    generateInt(arg1: number, arg2?: number): number {
         if (arg2 === undefined) {
             return Math.floor(this.random.generate() * arg1);
+        }
+        if (arg1 >= arg2) {
+            throw new Error("arg1 must be less than arg2.");
         }
         return Math.floor(this.random.generate() * (arg2 - arg1) + arg1);
     }
