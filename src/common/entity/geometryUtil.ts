c5878559c9af1eff904a8f5a9268b4577bd58910
klikki-lab <klikki.lab@gmail.com>
2026-08-14 16:21:57 +0900

    Transformable を削除

--- src/common/entity/geometryUtil.ts
+++ src/common/entity/geometryUtil.ts
@@ -1,22 +1,15 @@
-interface Transformable extends g.CommonArea {
-    scaleX: number;
-    scaleY: number;
-    anchorX: number;
-    anchorY: number;
-}
+export function getCenterX(e: g.Object2D): number { return getLeft(e) + getWidth(e) / 2; }
 
-export function getCenterX(e: Transformable): number { return getLeft(e) + getWidth(e) / 2; }
+export function getCenterY(e: g.Object2D): number { return getTop(e) + getHeight(e) / 2; }
 
-export function getCenterY(e: Transformable): number { return getTop(e) + getHeight(e) / 2; }
+export function getWidth(e: g.Object2D): number { return e.width * e.scaleX; }
 
-export function getWidth(e: Transformable): number { return e.width * e.scaleX; }
+export function getHeight(e: g.Object2D): number { return e.height * e.scaleY; }
 
-export function getHeight(e: Transformable): number { return e.height * e.scaleY; }
+export function getLeft(e: g.Object2D): number { return e.x - getWidth(e) * (e.anchorX ?? 0); }
 
-export function getLeft(e: Transformable): number { return e.x - getWidth(e) * e.anchorX; }
+export function getTop(e: g.Object2D): number { return e.y - getHeight(e) * (e.anchorY ?? 0); }
 
-export function getTop(e: Transformable): number { return e.y - getHeight(e) * e.anchorY; }
+export function getRight(e: g.Object2D): number { return getLeft(e) + getWidth(e); }
 
-export function getRight(e: Transformable): number { return getLeft(e) + getWidth(e); }
-
-export function getBottom(e: Transformable): number { return getTop(e) + getHeight(e); }
+export function getBottom(e: g.Object2D): number { return getTop(e) + getHeight(e); }
