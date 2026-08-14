7d8c69648e8147b1e9736dc0b7868894dbf1e0cb
klikki-lab <klikki.lab@gmail.com>
2026-08-14 16:29:05 +0900

    spaceFilledLabel.ts を追加

---
+++ src/common/hud/spaceFilledLabel.ts
@@ -0,0 +1,22 @@
+export interface SpaceFilledParameterObject extends g.LabelParameterObject {
+    /** 桁数。1 未満の場合、エラーを投げる。 */
+    digits: number;
+}
+
+export class SpaceFilledLabel extends g.Label {
+
+    #spaces: string;
+
+    constructor(param: SpaceFilledParameterObject) {
+        if (param.digits < 1) throw new Error(`digits は 1 以上である必要があります。 digits:${param.digits}`);
+        super(param);
+
+        this.#spaces = ' '.repeat(param.digits - 1);
+    }
+
+    setValue(value: number): void {
+        const spacesFiiledValue = (this.#spaces + value).slice(-(this.#spaces.length + 1));
+        this.text = `${spacesFiiledValue}`;
+        this.invalidate();
+    }
+}
